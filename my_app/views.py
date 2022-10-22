34a925c7ce3ff266bf70c35343b9bd2dbd5c1479
Drew Anello <drew.anello@gmail.com>
2022-10-22 18:11:56 -0400

    signup is working

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,26 @@
+from django.contrib.auth import login
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('home')
+
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
