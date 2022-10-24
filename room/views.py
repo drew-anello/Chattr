0a6b71e9f28bac1ee15b1f3f1ef4ef131e0224df
Drew Anello <drew.anello@gmail.com>
2022-10-24 17:19:37 -0400

    join room button is functional aswell as individual chat room

--- room/views.py
+++ room/views.py
@@ -9,3 +9,10 @@ def rooms(request):
     rooms = Room.objects.all()
 
     return render(request, 'room/rooms.html', {'rooms': rooms})
+
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+
+    return render(request, 'room/rooms.html', {'room': room})
