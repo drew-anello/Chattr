0c8025b27d5f47826a7607ce6e210cbfc1766e12
Drew Anello <drew.anello@gmail.com>
2022-10-24 20:33:24 -0400

    wip adds button functionality

--- chattr/settings.py
+++ chattr/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chattr.wsgi.application'
 ASGI_APPLICATION = 'chattr.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
